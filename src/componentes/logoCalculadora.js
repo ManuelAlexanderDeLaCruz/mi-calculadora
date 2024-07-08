0fb43f099b040a6dbe3668c25556e43cdeb2e6a4
ManuelAlexanderDeLaCruz <alexdelacruzcapellan@gmail.com>
2024-07-07 20:49:51 -0400

    primer commit

---
+++ src/componentes/logoCalculadora.js
@@ -0,0 +1,28 @@
+import React from "react";
+import calculadoraLogo from '../imagenes/imagenCalculadora1.jpg';
+import '../hojas-de-estilo/logoCalculadora.css';
+
+
+ export default function LogoUn () {
+  
+    return (<div > <img src={calculadoraLogo} className="calculadora-logo" alt='logo de calculadora' /> 
+    
+ </div>
+    );
+};
+
+
+
+////function LogoUno ( ) { 
+//    return (
+//<div className='calculadora-logo-contenedor'>    
+//       <img 
+//        src={calculadoraLogo}
+//        className='calculadora-logo'
+//       alt='logo de calculadora' />
+//      </div>  
+//    )
+//};
+
+
+  
